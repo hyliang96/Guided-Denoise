7cf520e1022c81085621348e385a4d108ef436b3
hyliang96 <hyliang96@hotmail.com>
2019-04-22 16:59:28 +0800

    add process_bar in DG_train/train.py

---
+++ GD_train/print_utlis.py
@@ -0,0 +1,111 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import sys
+import time
+
+# 支持在python2、3下调用本文件
+
+
+class Log(object):
+
+# 用例
+# 从log = Log('test_data')开始，print会向文件'test_data'与屏幕输出（双向输出）
+# 包括：
+#   - 凡print函数皆双向输出
+#     - 此处代码中的print
+#     - 被调用的函数中有print
+#     - 被import的文件中有print
+#   - 支持输出到文件、屏幕的flush()
+#   - 不支持'\r'输出到文件（vim下'\r'会显示为^M），支持'\r'输出到文件，故进度条只能在屏幕上显示
+# 直到log.close()，print才变为只输出到屏幕
+
+    def __init__(self, filename='',*args):
+        # filename =
+         # time.strftime("%m-%d_%H:%M:%S", time.localtime())+filename
+        self.f = open(filename, 'w')
+        sys.stdout = self
+        print('====== log start ======',
+            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+            # 格式化成2016-03-20 11:45:39形式
+            '======')
+
+    def write(self, data):
+        self.f.write(data)
+        sys.__stdout__.write(data)
+
+    def flush(self):
+        # 例
+        #   print("xxxx", end='')
+        #   log.flush()
+        # 可使"xxxx"立即输出到屏幕和文件；
+        # 若不"log.flush()"，则要等到之后有换行的 print("xxxx")
+        self.f.flush()
+        sys.__stdout__.flush()
+
+    def close(self):
+        print('======= log end =======',
+            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+            '======')
+        self.flush()
+        self.f.close()
+        sys.stdout=sys.__stdout__
+
+
+
+def cursor_back(func):
+    # 光标回到所在行首，且只输出到屏幕，不输出到别处
+        # 因为在屏幕是上，'\r'可令光标返回行首，但本行已输出的字符不会删除
+        # 但在文件中，'\r'显示为"^M" 或 "<0x0d>"，不能返回光标到行首
+    def func_with_cursor_back(*args, **kw):
+        # 以防之前stdout修改，如"屏幕、文件双向输出"功能
+        original = sys.stdout
+        sys.stdout = sys.__stdout__
+        # 光标回到所在行首
+        result = func(*args, **kw)
+        print("\r",end="")
+        sys.stdout.flush()
+        # 还原sys.stdout
+        sys.stdout.flush()
+        sys.stdout = original
+
+        return result
+    return func_with_cursor_back
+
+
+
+def progess_bar(process_rate, precision=2,num_block=30,prefix="",suffix="", clean=True):
+    # 输出到屏幕
+        # process_rate 是[0,1]的float，表示百分之多少的进度
+        # precision小数点位数
+        # 进度条的'>'块数
+    # 效果
+        #  94.000 %   | > > > > > > > > > > > > > > > > > >     |
+    # clean:    当process_rate == 1时怎么结束
+        # Ture:  清除进度条，不换行，光标回行头
+        # False: 保留进度条，直接换行
+
+    line=""
+    line+=prefix
+    # 右对齐输出百分数
+    line+= ( ('%%%ds'%(precision+4))  % (('%%.%df'%precision)%(100*process_rate)) ) +' %'
+    line+='   |'
+    # 输出进度条
+    n = int(process_rate*num_block)
+    for i in range(n):
+        line+='>'
+    for i in range(num_block-n):
+        line+=' '
+    line+='|   '
+    # 后缀
+    line+=suffix
+
+    cursor_back(print)(line, end='')
+
+    if process_rate==1:
+        if clean:
+            cursor_back(print)(' '*len(line), end='')
+        else:
+            print()
+
--- GD_train/train.py
+++ GD_train/train.py
@@ -5,6 +5,38 @@ import torch
 from torch.autograd import Variable
 from torch import optim
 from torch.nn import DataParallel
+from print_utlis import progess_bar
+
+def tablecell(head,data,leng,form,ifhead,seg='  '):
+    if ifhead:
+        return ('{:>%s}'%leng).format(head)+seg
+    elif data == '':
+        return ('{:>%s}'%leng).format('')+seg
+    else:
+        return ('{:>%s%s}'%(leng,form)).format(data)+seg
+
+def output_state(state='', epoch='', orig_acc='', adv_acc='',
+          dt='', time_per_batch='', time_per_img='',
+          lr='', loss='', ifhead=False):
+
+    line =\
+        tablecell('state',state,10,'s', ifhead)+\
+        tablecell('epoch',epoch,10,'d', ifhead)+\
+        tablecell('orig_acc',orig_acc*100,10,'.3f', ifhead)+\
+        tablecell('adv_acc',adv_acc*100,10,'.3f', ifhead)+\
+        tablecell('all_time/s',dt, 10,'.1f', ifhead)+\
+        tablecell('tbatch/ms',time_per_batch, 10, '.1f', ifhead)+\
+        tablecell('timg/ms',time_per_img*1000, 10,'.3f', ifhead)+\
+        tablecell('lr',lr,10,'.7f', ifhead)
+
+    if ifhead:
+        line+='loss per level'
+    elif type(loss)==str and loss=='':
+        line+=""
+    else:
+        line+=''.join(['%.5f  ' % loss_i for loss_i in loss])
+
+    print(line)
 
 def train(epoch, net, data_loader, optimizer, get_lr, loss_idcs = [4], requires_control = True):
     start_time = time.time()
@@ -24,7 +56,11 @@ def train(epoch, net, data_loader, optimizer, get_lr, loss_idcs = [4], requires_
     if requires_control:
         control_acc = []
         control_loss = []
+
+    n_batch = len(data_loader)
     for i, (orig, adv, label) in enumerate(data_loader):
+        progess_bar((i+1)/n_batch, precision=4,num_block=70, prefix='train',
+            suffix="Batch %4d / %4d"%(i+1, n_batch), clean=True)
         # orig = Variable(orig.cuda(async = True), volatile = True)
         # adv = Variable(adv.cuda(async = True), volatile = True)
         orig = Variable(orig.cuda(async = True))
@@ -47,7 +83,7 @@ def train(epoch, net, data_loader, optimizer, get_lr, loss_idcs = [4], requires_
         optimizer.step()
         loss_values = []
         for ll in l:
-            loss_values.append(ll.mean().data[0])
+            loss_values.append(ll.mean().item())
         loss.append(loss_values)
 
         if requires_control:
@@ -55,7 +91,7 @@ def train(epoch, net, data_loader, optimizer, get_lr, loss_idcs = [4], requires_
             control_acc.append(float(torch.sum(idcs == label)) / len(label))
             loss_values = []
             for ll in cl:
-                loss_values.append(ll.mean().data[0])
+                loss_values.append(ll.mean().item())
             control_loss.append(loss_values)
 
         #print('\torig_acc %.3f, acc %.3f, control_acc %.3f' % (
@@ -70,26 +106,37 @@ def train(epoch, net, data_loader, optimizer, get_lr, loss_idcs = [4], requires_
     orig_acc = np.mean(orig_acc)
     acc = np.mean(acc)
     loss = np.mean(loss, 0)
+
     if requires_control:
         control_acc = np.mean(control_acc)
         control_loss = np.mean(control_loss, 0)
     end_time = time.time()
     dt = end_time - start_time
 
-    if requires_control:
-        print('Epoch %3d (lr %.5f): orig_acc %.3f, acc %.3f, control_acc %.3f, time %3.1f' % (
-            epoch, lr, orig_acc, acc, control_acc, dt))
-    else:
-        print('Epoch %3d (lr %.5f): orig_acc %.3f, acc %.3f, time %3.1f' % (
-            epoch, lr, orig_acc, acc, dt))
+    n_go = 3 if requires_control else 2
+    time_per_batch = dt/(n_batch * n_go)
+    time_per_img = dt/(data_loader.batch_size * n_batch *  n_go)
 
-    print('\tloss: %.5f, %.5f, %.5f' % (
-        loss[0], loss[1], loss[2]))
-
+    output_state(state='train', epoch=epoch, orig_acc=orig_acc, adv_acc=acc,
+                 dt=dt, time_per_batch=time_per_batch, time_per_img=time_per_img,lr=lr, loss=loss)
     if requires_control:
-        print('\tloss: %.5f, %.5f, %.5f' % (
-            control_loss[0], control_loss[1], control_loss[2]))
-    print
+        output_state(state=' - defense', adv_acc=control_acc, loss=control_loss)
+
+
+    # if requires_control:
+    #     print('Epoch %3d (lr %.5f): orig_acc %.3f, acc %.3f, control_acc %.3f, time %3.1f' % (
+    #         epoch, lr, orig_acc, acc, control_acc, dt))
+    # else:
+    #     print('Epoch %3d (lr %.5f): orig_acc %.3f, acc %.3f, time %3.1f' % (
+    #         epoch, lr, orig_acc, acc, dt))
+
+    # print('\tloss: %.5f, %.5f, %.5f' % (
+    #     loss[0], loss[1], loss[2]))
+
+    # if requires_control:
+    #     print('\tloss: %.5f, %.5f, %.5f' % (
+    #         control_loss[0], control_loss[1], control_loss[2]))
+    # print
 
 def val(epoch, net, data_loader, requires_control = True):
     start_time = time.time()
@@ -101,7 +148,12 @@ def val(epoch, net, data_loader, requires_control = True):
     if requires_control:
         control_acc = []
         control_loss = []
+
+    n_batch = len(data_loader)
     for i, (orig, adv, label) in enumerate(data_loader):
+        progess_bar((i+1)/n_batch, precision=4, num_block=70, prefix='val  ',
+            suffix="Batch %4d / %4d"%(i+1, n_batch), clean=True)
+
         # orig = Variable(orig.cuda(async = True), volatile = True)
         # adv = Variable(adv.cuda(async = True), volatile = True)
         orig = Variable(orig.cuda(async = True))
@@ -118,7 +170,7 @@ def val(epoch, net, data_loader, requires_control = True):
         acc.append(float(torch.sum(idcs == label)) / len(label))
         loss_values = []
         for ll in l:
-            loss_values.append(ll.mean().data[0])
+            loss_values.append(ll.mean().item())
         loss.append(loss_values)
 
         if requires_control:
@@ -126,7 +178,7 @@ def val(epoch, net, data_loader, requires_control = True):
             control_acc.append(float(torch.sum(idcs == label)) / len(label))
             loss_values = []
             for ll in cl:
-                loss_values.append(ll.mean().data[0])
+                loss_values.append(ll.mean().item())
             control_loss.append(loss_values)
 
         #print('\torig_acc %.3f, acc %.3f, control_acc %.3f' % (
@@ -147,21 +199,33 @@ def val(epoch, net, data_loader, requires_control = True):
     end_time = time.time()
     dt = end_time - start_time
 
-    if requires_control:
-        print('Validation: orig_acc %.3f, acc %.3f, control_acc %.3f, time %3.1f' % (
-            orig_acc, acc, control_acc, dt))
-    else:
-        print('Validation: orig_acc %.3f, acc %.3f, time %3.1f' % (
-            orig_acc, acc, dt))
 
-    print('\tloss: %.5f, %.5f, %.5f' % (
-        loss[0], loss[1], loss[2]))
+    n_go = 3 if requires_control else 2
+    time_per_batch = dt/(n_batch * n_go)
+    time_per_img = dt/(data_loader.batch_size * n_batch *  n_go)
 
+
+    output_state(state='val', orig_acc=orig_acc, adv_acc=acc,
+                 dt=dt, time_per_batch=time_per_batch, time_per_img=time_per_img, loss=loss)
     if requires_control:
-        print('\tloss: %.5f, %.5f, %.5f' % (
-            control_loss[0], control_loss[1], control_loss[2]))
-    print
-    print
+        output_state(state=' - defense', adv_acc=control_acc, loss=control_loss)
+
+
+    # if requires_control:
+    #     print('Validation: orig_acc %.3f, acc %.3f, control_acc %.3f, time %3.1f' % (
+    #         orig_acc, acc, control_acc, dt))
+    # else:
+    #     print('Validation: orig_acc %.3f, acc %.3f, time %3.1f' % (
+    #         orig_acc, acc, dt))
+
+    # print('\tloss: %.5f, %.5f, %.5f' % (
+    #     loss[0], loss[1], loss[2]))
+
+    # if requires_control:
+    #     print('\tloss: %.5f, %.5f, %.5f' % (
+    #         control_loss[0], control_loss[1], control_loss[2]))
+    # print
+    # print
 
 
 def test(net, data_loader, result_file_name, defense = True):
